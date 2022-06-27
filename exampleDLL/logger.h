a4cd44a0a2d0eec52ba81bab560dbf96c878ded6
basicallygit <91993321+basicallygit@users.noreply.github.com>
2022-06-27 17:40:06 +0000

    fixed class referenced before defenition

--- exampleDLL/logger.h
+++ exampleDLL/logger.h
@@ -4,6 +4,13 @@
 #include <chrono>
 std::string getDateTime();
 
+class Log {
+    public:
+        static std::string time;
+        static std::string type; //info || warning || error
+        static std::string message;
+};
+
 void logger(Log loginfo);
 
 #endif
